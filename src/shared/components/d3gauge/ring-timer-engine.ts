9233a403e485e0672df5e6d75ae62b9da29599b1
Bruno Muniz <bruno@MacBook-Pro-de-Bruno.local>
2019-03-30 14:56:46 -0300

    ringer timer engine clean

--- src/shared/components/d3gauge/ring-timer-engine.ts
+++ src/shared/components/d3gauge/ring-timer-engine.ts
@@ -1,6 +1,6 @@
 import { Subject } from 'rxjs/Subject';
 
-import * as d3 from 'd3'; // for the interval function
+import * as d3 from 'd3'; // para a função de intervalo
 
 import { TimerTimeData } from './ring-timer-config.interface';
 
@@ -11,14 +11,14 @@ export class RingTimerEngine {
   public initSubject: Subject<any>;
   public pausePingSubject: Subject<any>;
   public finishedSubject: Subject<any>;
-  
+
   private warmUpFor: number;
   private countdownFor: number;
   private warningFor: number;
   private countdownToTime: number;
   private countdownRemaining: number;
   private warmUpRemaining: number;
-  
+
   private myD3Timer: any;
   private config: any;
 
@@ -38,27 +38,27 @@ export class RingTimerEngine {
     //const DAYS_IN_YEAR: number = 365;
 
     this.timeData = [
-      {idx: 0, t: 'MILLISEC', s: MILLISECS_IN_MILLISEC, max: MILLISECS_IN_SECOND, value: 0, baseZeroToOne: 0, singleDecValue: 0},
-      {idx: 1, t: 'SEGUNDOS', s: MILLISECS_IN_SECOND,  max: SECONDS_IN_MINUTE, value: 0, baseZeroToOne: 0, singleDecValue: 0},
-      {idx: 2, t: 'MINUTES', s: MILLISECS_IN_MINUTE, max: MINUTES_IN_HOUR, value: 0, baseZeroToOne: 0, singleDecValue: 0},
-     // {idx: 3, t: 'HOURS', s: MILLISECS_IN_HOUR, max: HOURS_IN_DAY, value: 0, baseZeroToOne: 0, singleDecValue: 0},
-     // {idx: 4, t: 'DAYS', s: MILLISECS_IN_DAY, max: DAYS_IN_YEAR, value: 0, baseZeroToOne: 0, singleDecValue: 0}
+      { idx: 0, t: 'MILLISEC', s: MILLISECS_IN_MILLISEC, max: MILLISECS_IN_SECOND, value: 0, baseZeroToOne: 0, singleDecValue: 0 },
+      { idx: 1, t: 'SEGUNDOS', s: MILLISECS_IN_SECOND, max: SECONDS_IN_MINUTE, value: 0, baseZeroToOne: 0, singleDecValue: 0 },
+      { idx: 2, t: 'MINUTES', s: MILLISECS_IN_MINUTE, max: MINUTES_IN_HOUR, value: 0, baseZeroToOne: 0, singleDecValue: 0 },
+      // {idx: 3, t: 'HOURS', s: MILLISECS_IN_HOUR, max: HOURS_IN_DAY, value: 0, baseZeroToOne: 0, singleDecValue: 0},
+      // {idx: 4, t: 'DAYS', s: MILLISECS_IN_DAY, max: DAYS_IN_YEAR, value: 0, baseZeroToOne: 0, singleDecValue: 0}
     ]
   }
 
   loadDefaultConfig() {
     let config = {
-      updateInterval: 31, // ms time between updates to text and micTimer arcs.
-      timeUnitCount: 5, // number of micTimers in the component
-      countdownFor: 15000,  // countdown time in ms
-      //warmUpFor: 3000,  // countdown time in ms
-     // warningFor: 1000,  // countdown time in ms
-      
-      calc: { // these variables are placed here as the config structure is a convenient place to store, pass them. But not actually configuration variables.
-          phase: 'ready', // 'warmup', 'countdown', 'warning', 'finished', 'paused', 'stopped'
-          //previousPhase: 'ready', // 'warmup', 'countdown', 'warning', 'finished', 'paused', 'stopped'
-         // warmUpMessage: 'ready...'
-      } 
+      updateInterval: 31, // ms tempo entre atualizações para texto e arcos micTimer.
+      timeUnitCount: 5, // número de micTimers no componente
+      countdownFor: 15000,  // tempo de contagem regressiva em ms
+      //warmUpFor: 3000,  // tempo de contagem regressiva em ms
+      // warningFor: 1000,  // tempo de contagem regressiva em ms
+
+      calc: { // essas variáveis ​​são colocadas aqui, pois a estrutura de configuração é um local conveniente para armazenar, passá-las. Mas não, na verdade, variáveis ​​de configuração.
+        phase: 'ready', // 'warmup', 'countdown', 'warning', 'finished', 'paused', 'stopped'
+        //previousPhase: 'ready', // 'warmup', 'countdown', 'warning', 'finished', 'paused', 'stopped'
+        // warmUpMessage: 'pronto...'
+      }
     };
     this.config = config;
   }
@@ -68,7 +68,7 @@ export class RingTimerEngine {
     this.loadDefaultConfig();
   }
 
-  private initSubjects(){
+  private initSubjects() {
     this.timeDataSubject = new Subject<any>();
     this.phaseSubject = new Subject<any>();
     this.initSubject = new Subject<any>();
@@ -77,12 +77,12 @@ export class RingTimerEngine {
   }
 
   // ** 
-  // ** public getters for Observers to pull data
+  // ** captadores públicos para os observadores puxarem dados
   // **
   public getTimeData(): any {
     return this.timeData;
   }
-  
+
   public getPhase(): string {
     return this.config.calc.phase;
   }
@@ -96,20 +96,20 @@ export class RingTimerEngine {
   }
 
   // ** 
-  // ** public setter for component
+  // ** setter público para componente
   // **
   public setWarmUpFor(warmUpFor: number): void {
     this.warmUpFor = warmUpFor;
-  }  
+  }
   public setCountdownFor(countdownFor: number): void {
     this.countdownFor = countdownFor;
-  }  
+  }
   public setWarningFor(warningFor: number): void {
     this.warningFor = warningFor;
-  }  
+  }
 
   // **
-  // ** Load Timer's Time Settings
+  // ** Carregar as configurações de tempo do timer
   // **
   private loadParentConfig(): void {
     this.config.warmUpFor = this.warmUpFor;
@@ -118,10 +118,10 @@ export class RingTimerEngine {
   }
 
   // **
-  // ** Timer functions
+  // ** Funcoes do timer
   // **
-  
-  // Timer Initialisation
+
+  // Timer Inicializacao
   public initTimer(): void {
     console.log('in initTimer');
     this.readyTimer();
@@ -129,37 +129,37 @@ export class RingTimerEngine {
   }
 
   private readyTimer(): void {
-    // load configuration data
+    // carregar dados de configuração
     this.loadDefaultConfig();
     this.loadParentConfig();
-    // load fresh Time Data object
+    //carregar o objeto de dados de hora novo
     this.initRingerTimeData();
-    // calculate the time to run the timer
+    // calcular o tempo para executar o temporizador
     this.setCountdownToTime();
-    // setup the number of time units to process.
+    // configurar o número de unidades de tempo a processar.
     this.calcInitialTimeUnitCount();
     this.pruneTimeDataArray();
-    
+
     this.setPhaseByValue('ready');
     let time = this.setTimeRemainingAndPhase();
     this.updateTimeData(time);
   }
 
-  // Public Timer Controls
+  // Controles de timer público
   public startTimer(): void {
     if (this.config.calc.phase !== 'ready')
-        return;
-    // move from ready to warmup;
+      return;
+    // passar de pronto para aquecer;
     this.setPhaseByValue('warmup');
     this.setCountdownToTime();
-    // start the timer loop
+    // iniciar o loop temporizador
     this.runTimer();
   }
 
   private runTimer(): void {
     let self = this;
-    this.myD3Timer = d3.interval(function(elapsed) {
-      // check if time is up or if it has been stopped by the user.
+    this.myD3Timer = d3.interval(function (elapsed) {
+      // verifique se o tempo acabou ou se foi interrompido pelo usuário.
       if (self.isTimerFinishedOrStopped()) {
         self.myD3Timer.stop();
         self.update();
@@ -184,7 +184,7 @@ export class RingTimerEngine {
     this.runTimer();
   }
 
-  public stopTimer(): void  {
+  public stopTimer(): void {
     this.myD3Timer.stop();
     this.setPhaseByValue('stopped');
     this.runTimer();
@@ -202,26 +202,24 @@ export class RingTimerEngine {
   }
 
   // **
-  // ** update is called by the main timer interval, it simply calls the methods required to
-  // ** update the time information and redraw the timer components d3 objects.
+  // ** atualização é chamada pelo intervalo do temporizador principal, ele simplesmente chama os métodos necessários para
+  // ** atualize as informações de hora e redesenhe os objetos d3 dos componentes do temporizador.
   // **
   private update(): void {
     let time = this.setTimeRemainingAndPhase();
-    
+
     if (!this.isTimerPaused())
       this.updateTimeData(time);
-    else 
+    else
       this.pausePing();
   }
 
-  // TODO refactor this long multi-purpose function
-  // TODO remove switch statement, may move to polymorphic or delegation solution.
   private setTimeRemainingAndPhase(): number {
     let time: number = 0;
 
-    // store the new time remaining and exit
-    // check that we aren't already paused, stopped or finished.
-    switch(this.config.calc.phase) {
+    // guarde o novo tempo restante e saia
+    // Verifique se já não estamos em pausa, paramos ou terminamos.
+    switch (this.config.calc.phase) {
       case 'ready':
         this.countdownRemaining = this.config.countdownFor + this.config.warmUpFor;
         time = this.config.countdownFor;
@@ -232,10 +230,10 @@ export class RingTimerEngine {
         time = Math.max(0, this.countdownToTime - (new Date().getTime()));
         this.countdownRemaining = time;
         this.setPhase(this.countdownRemaining);
-        if (this.config.calc.phase === 'warmup') { 
-          // initially the countdown time was displayed during warmup - it has now been replaced by text.
+        if (this.config.calc.phase === 'warmup') {
+          // Inicialmente, o tempo de contagem regressiva foi exibido durante o aquecimento - ele foi substituído pelo texto.
           time = Math.min(time, this.config.countdownFor);
-          // over the period of a second, during the warm up phase, we want the alpha component of the countdown rings and text to rise from 0 to 1.
+          // Durante o período de um segundo, durante a fase de aquecimento, queremos que o componente alfa dos anéis de contagem regressiva e o texto subam de 0 para 1.
           this.warmUpRemaining = Math.max(0, this.countdownRemaining - this.config.countdownFor);
         };
         break;
@@ -245,9 +243,8 @@ export class RingTimerEngine {
       case 'stopped':
         this.countdownRemaining = 0;
         break;
-      default :
-        // typically would throw error here rather than console.log
-        console.log('error we have an unrecognised timer status')
+      default:
+      // normalmente lançaria erro aqui em vez de console.log
     }
     return time;
   }
@@ -255,10 +252,10 @@ export class RingTimerEngine {
   private updateTimeData(time: number): void {
     let timeInMS = time;
 
-    for (let i=this.timeData.length-1; i>=0; i--) {
+    for (let i = this.timeData.length - 1; i >= 0; i--) {
       let td = this.timeData[i];
       let value: number = time / td.s;
-      td.singleDecValue = timeInMS  / td.s;
+      td.singleDecValue = timeInMS / td.s;
       time -= Math.floor(value) * td.s;
       td.value = Math.floor(value);
       td.baseZeroToOne = (td.value / td.max);
@@ -271,7 +268,7 @@ export class RingTimerEngine {
     this.timeDataChanged();
   }
 
-  // Notify Observers of change
+  // Notificar observadores de mudança
   private timeDataChanged(): void {
     if (this.timeDataSubject !== undefined)
       this.timeDataSubject.next(true);
@@ -297,7 +294,7 @@ export class RingTimerEngine {
       this.finishedSubject.next(true);
   }
 
-  // Pull a phase change into the queue
+  // Puxe uma mudança de fase para a fila
   private setPhaseByValue(phase): void {
     let t = d3.timer(() => {
       this.config.calc.phase = phase;
@@ -306,10 +303,9 @@ export class RingTimerEngine {
     }, 0);
   };
 
-  // Set the phase based on the timer's time.
-  // TODO remove switch statement, may move to polymorphic or delegation solution.
+  // Defina a fase com base no tempo do timer.
   private setPhase(timeRemaining): void {
-    // this should only be calls when the timer is actively running (i.e. phase warmup, countdown and warning)
+    // isso só deve ser chamado quando o cronômetro estiver sendo executado ativamente (por exemplo, aquecimento de fase, contagem regressiva e aviso)
     if (this.config.calc.phase === 'warmup' || this.config.calc.phase === 'countdown' || this.config.calc.phase === 'warning') {
       if (timeRemaining > this.config.countdownFor) {
         this.setPhaseByValue('warmup');
@@ -317,12 +313,10 @@ export class RingTimerEngine {
         this.setPhaseByValue('countdown');
       } else if ((timeRemaining <= this.config.warningFor) && (timeRemaining > this.config.updateInterval)) {
         this.setPhaseByValue('warning');
-      } else if (timeRemaining <= this.config.updateInterval){
-        // less than an interval to go - let's finish up.
+      } else if (timeRemaining <= this.config.updateInterval) {
+        // menos de um intervalo para ir - vamos terminar.
         this.setPhaseByValue('finished');
       } else {
-        // typically would throw error here rather than console.log
-        console.log('Error in ring-timer.component.ts=>setPhase')
       }
     }
   }
@@ -336,7 +330,7 @@ export class RingTimerEngine {
   }
 
   // **
-  // ** Determine number of Time Units to be processed
+  // ** Determinar o número de unidades de tempo a serem processadas
   // **
   private calcInitialTimeUnitCount(): void {
     let time: number = Math.abs(this.countdownToTime - (new Date().getTime()) - +this.config.warmUpFor);
@@ -344,18 +338,17 @@ export class RingTimerEngine {
     console.log('in calcInitialTimeUnitCount=>time: ' + time);
     console.log('in calcInitialTimeUnitCount=>getTime: ' + new Date().getTime());
     console.log('in calcInitialTimeUnitCount=>this.timeData.length: ' + this.timeData.length);
-    for (let timeUnitCount=this.timeData.length-1; timeUnitCount>=0; timeUnitCount--) {
+    for (let timeUnitCount = this.timeData.length - 1; timeUnitCount >= 0; timeUnitCount--) {
       let td = this.timeData[timeUnitCount];
-      if (time > td.s) 
+      if (time > td.s)
         this.config.timeUnitCount = (timeUnitCount + 1);
     }
-    console.log('in calcInitialTimeUnitCount=>this.config.timeUnitCount: ' + this.config.timeUnitCount);
   }
 
   private pruneTimeDataArray(): void {
-    // remove unneeded rows from the timeData array.
+    // remova linhas desnecessárias da matriz timeData.
     let arrSize = this.timeData.length;
-    for (let i=arrSize-1; i>this.config.timeUnitCount; i--) {
+    for (let i = arrSize - 1; i > this.config.timeUnitCount; i--) {
       this.timeData.pop();
     }
   }
