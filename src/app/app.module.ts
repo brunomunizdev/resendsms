2d536c712e2cb8bb8ce8be5d94f75728cb3eb869
Bruno Muniz <bruno@MacBook-Pro-de-Bruno.local>
2019-03-30 14:21:02 -0300

    Counter to resend received sms.

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { RingTimerComponent } from '../shared/components/d3gauge/ring-timer.component';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    RingTimerComponent
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
